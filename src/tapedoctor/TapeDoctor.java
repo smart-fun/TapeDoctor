6ded0a9854c247d1ce1a1a0264f4534dff4cc4b3
Arnaud Guyon <arnaud.guyon@b-i.com>
2018-05-31 22:53:06 +0200

    Build 0.8.5

--- src/tapedoctor/TapeDoctor.java
+++ src/tapedoctor/TapeDoctor.java
@@ -32,7 +32,7 @@ import tapedoctor.WavFile.MissingBitInfo;
  */
 public class TapeDoctor extends Application implements Menus.OnMenuListener, WavImage.OnWavImageListener {
     
-    public static final String VERSION = "0.8.0";
+    public static final String VERSION = "0.8.5";
     
     private VBox root;
     private Stage stage;
